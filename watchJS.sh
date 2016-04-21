e0d5cbd75d02a99a6ae419dba29f9e43f4fad58c
Jeff Torson <jeff.torson@objectpartners.com>
2016-04-21 13:14:38 -0500

    Change watchJS script to use nodejs in project directory instead of user directory

--- watchJS.sh
+++ watchJS.sh
@@ -5,7 +5,7 @@
 # The version of node must match what's in gradle.
 
 NODE_VERSION="5.8.0"
-NODE_LOC="${HOME}/.gradle/nodejs/node-v${NODE_VERSION}-darwin-x64/bin/node"
+NODE_LOC="./.gradle/nodejs/node-v${NODE_VERSION}-darwin-x64/bin/node"
 
 chmod u+x ${NODE_LOC}
 ${NODE_LOC} ./node_modules/.bin/webpack --watch --display-error-details
