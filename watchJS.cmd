87be7fdffd9d040d051eb7f7d7f140bac31088ea
Jeff Torson <jeff.torson@objectpartners.com>
2016-05-25 22:45:38 -0500

    Added a Windows watchJS.cmd script file

--- watchJS.cmd
+++ watchJS.cmd
@@ -1,6 +1,10 @@
 @echo off
 REM This is a simple helper script used to start the webpack watch process
 REM so that javascript files can be automatically reloaded when they are saved.
+REM The version of node must match what's in gradle.
 
-SET WEBPACK_LOC=node_modules\.bin\webpack
-%~dp0%WEBPACK_LOC% --watch --display-error-details
+SET NODE_VERSION=5.8.0
+SET NODE_LOC=.gradle\nodejs\node-v%NODE_VERSION%-windows-x64\bin\node
+SET WEBPACK_LOC=node_modules\webpack\bin\webpack.js
+
+%~dp0%NODE_LOC% %~dp0%WEBPACK_LOC% --watch --display-error-details
